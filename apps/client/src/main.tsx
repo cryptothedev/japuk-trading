b5c26e0d454f46437b49b9f2feabd90a58b7470c
cryptodev <crypto.the.dev@gmail.com>
2023-06-20 20:35:31 +0700

    feat: chakra setup

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
+import { theme } from './configs/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
